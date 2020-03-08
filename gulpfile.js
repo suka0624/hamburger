5f4022d968a15e714b385dc05438f16420109408
Shintaro Suka <suka0624suka@gmail.com>
2020-03-08 10:33:35 +0900

    SASS環境構築、SASS->CSS変換確認

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+gulp.task('sass:watch', () => {
+  gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+})
