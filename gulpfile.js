d29f8b6b19c1be625779038599e4a577a3fea340
Shintaro Suka <suka0624suka@gmail.com>
2020-03-08 20:46:49 +0900

    normalize.cssの追加、READMEの修正

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,17 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var packageImporter = require('node-sass-package-importer');
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
   return gulp.src('./asset/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
+    .pipe(sass({
+      importer: packageImporter({
+        extensions: ['.scss', '.css']
+      })
+    }).on('error', sass.logError))
     .pipe(gulp.dest('./asset/css'));
 });
 gulp.task('sass:watch', () => {
